5b5f972c8909a2dc354088b27886a7825b6c6fac
Robt291610 <158675065+Robt291610@users.noreply.github.com>
2026-02-14 18:47:14 -0400

    add employee section updated, now it works when adding an employee

--- FrontEnd/human_resources_app/src/employees/AddEmployee.jsx
+++ FrontEnd/human_resources_app/src/employees/AddEmployee.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
 
 export default function () {
+
+    const[employee, setEmployee]=useState({
+        name:"",
+        department:"",
+        salary:""
+    })
+
+    const{name, department, salary } = employee
+
+    const onImputChange = (e) => {
+        setEmployee({...employee, [e.target.name]: e.target.value})
+    }
+
+    let navigation = useNavigate();
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const baseUrl = "http://localhost:8080/api/employees";
+        await axios.post(baseUrl, employee);
+        navigation('/');
+
+    }
+
+
+    
     return (
         <div className='container'>
 
@@ -9,20 +36,27 @@ export default function () {
             </div>
 
 
-            <form>
+            <form onSubmit={(e) => onSubmit(e)}>
+                
                 <div class="mb-3">
                     <label htmlFor="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="name" required={true}/>
+                    <input type="text" className="form-control" id="name" name="name" required={true}
+                        value={name} onChange={(e) => onImputChange(e)}
+                    />
                 </div>
 
                 <div class="mb-3">
                     <label htmlFor="department" classNema="form-label">Department</label>
-                    <input type="text" className="form-control" id="department" name="department"/>
+                    <input type="text" className="form-control" id="department" name="department"
+                        value={department} onChange={(e) => onImputChange(e)}
+                    />
                 </div>
 
                 <div class="mb-3">
                     <label htmlFor="salary" className="form-label">Salary</label>
-                    <input type="text" step="any" className="form-control" id="salary" name="salary"/>
+                    <input type="text" step="any" className="form-control" id="salary" name="salary"
+                        value={salary} onChange={(e) => onImputChange(e)}
+                    />
                 </div>
 
                 <div className="text-center">
@@ -30,8 +64,6 @@ export default function () {
                     <a href="/" className="btn btn-danger btn-sm me-3">Return</a>
                 </div>
             </form>
-
-
         </div>
     )
 }
