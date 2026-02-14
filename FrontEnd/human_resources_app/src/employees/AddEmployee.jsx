4ad6b00049468914698c20f33a8ccd9610cf8afa
Robt291610 <158675065+Robt291610@users.noreply.github.com>
2026-02-14 18:21:07 -0400

    Add form updated in order to add name, department and salary

--- FrontEnd/human_resources_app/src/employees/AddEmployee.jsx
+++ FrontEnd/human_resources_app/src/employees/AddEmployee.jsx
@@ -11,10 +11,24 @@ export default function () {
 
             <form>
                 <div class="mb-3">
-                    <label htmlFor="name" class="form-label">Name</label>
-                    <input type="email" class="form-control" id="name" required/>
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="name" required={true}/>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+
+                <div class="mb-3">
+                    <label htmlFor="department" classNema="form-label">Department</label>
+                    <input type="text" className="form-control" id="department" name="department"/>
+                </div>
+
+                <div class="mb-3">
+                    <label htmlFor="salary" className="form-label">Salary</label>
+                    <input type="text" step="any" className="form-control" id="salary" name="salary"/>
+                </div>
+
+                <div className="text-center">
+                    <button type="submit" className="btn btn-warning btn-sm">Add</button>
+                    <a href="/" className="btn btn-danger btn-sm me-3">Return</a>
+                </div>
             </form>
 
 
